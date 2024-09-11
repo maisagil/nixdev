3fa274ea20a92e96b7eda65512077e9caebc91ca
Eduardo Loback <eduardolloback@gmail.com>
2024-09-10 21:54:28 -0300

    feat: export as modules

--- flake.nix
+++ flake.nix
@@ -1,42 +1,14 @@
 {
-  inputs = {
-    nixpkgs.url = "github:cachix/devenv-nixpkgs/rolling";
-    systems.url = "github:nix-systems/default";
-    devenv.url = "github:cachix/devenv";
-    devenv.inputs.nixpkgs.follows = "nixpkgs";
-    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
-    fenix.url = "github:nix-community/fenix";
-    fenix.inputs.nixpkgs.follows = "nixpkgs";
-  };
-
-  nixConfig = {
-    extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
-    extra-substituters = "https://devenv.cachix.org";
-  };
-
-  outputs = { self, nixpkgs, devenv, systems, ... } @ inputs:
-    let
-      forEachSystem = nixpkgs.lib.genAttrs (import systems);
-    in
+  outputs = inputs:
     {
-      packages = forEachSystem (system: {
-        devenv-up = self.devShells.${system}.default.config.procfileScript;
-      });
-
-      devShells = forEachSystem
-        (system:
-          let
-            pkgs = nixpkgs.legacyPackages.${system};
-          in
-          {
-            default = devenv.lib.mkShell {
-              inherit inputs pkgs;
-              modules = [
-                ./modules/common.nix
-                ./modules/sops.nix
-                ./modules/process-compose.nix
-              ];
-            };
-          });
+      devenvModules = { ... }:
+        {
+          imports = [
+            ./modules/common.nix
+            ./modules/sops.nix
+            ./modules/process-compose.nix
+            ./languages/top-level.nix
+          ];
+        };
     };
 }

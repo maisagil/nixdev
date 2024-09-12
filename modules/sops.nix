f9f4d248de77ddddc1a63d523d16d1f48fe4fec7
Eduardo Loback <eduardolloback@gmail.com>
2024-09-12 12:00:05 -0300

    fix: sops

--- modules/sops.nix
+++ modules/sops.nix
@@ -9,20 +9,16 @@ in
 {
   options = {
     custom.sops = {
-      enable = lib.mkOption {
-        type = lib.types.bool;
-        default = false;
-        description = "Enable the module";
-      };
-      env_as_config_path = lib.mkOption {
+      enable = lib.mkEnableOption "Enable sops";
+      env_as_config.target_path = lib.mkOption {
         type = lib.types.str;
-        default = ".";
-        description = "Path to store the unencrypted environment secrets";
+        default = "./config";
+        description = "Path to save the unencrypted file.";
       };
-      env_as_config_script = lib.mkOption {
+      env_as_config.script = lib.mkOption {
         type = lib.types.str;
-        default = "${pkgs.sops}/bin/sops -d ${cfg.common.environment}.secrets.yaml > ${cfg.sops.env_as_config_path}/unencrypted_${cfg.common.environment}_secrets.yaml";
-        description = "Script to populate environment secrets";
+        default = "${pkgs.sops}/bin/sops -d ${cfg.common.environment}.secrets.yaml > ${cfg.sops.env_as_config.target_path}/unencrypted_${cfg.common.environment}_secrets.yaml";
+        description = "Populate secrets as a unencrypted file.";
       };
     };
   };
@@ -35,7 +31,7 @@ in
       scripts = {
         sops_populate_config.exec = ''
           echo "Populating environment secrets as config";
-          ${cfg.sops.env_as_config_script}
+          ${cfg.sops.env_as_config.script}
         '';
       };
     };

ce66b980f8686566ded714e923160642a14b0ee9
Eduardo Loback <eduardolloback@gmail.com>
2024-09-13 17:51:44 -0300

    chore: revert to old pup script

--- modules/process-compose.nix
+++ modules/process-compose.nix
@@ -13,19 +13,20 @@ in
       fields_order = [ "time" "process" "level" "message" ];
       log_location = ".devenv/process-compose.log";
       log_configuration = {
-        disable_json = false;
+        disable_json = true;
         add_timestamp = true;
         flush_each_line = true;
         rotation = {
-          max_size_mb = 1; # the max size in MB of the logfile before it's rolled
+          max_size_mb = 1; # the max size in mb of the logfile before it's rolled
           max_age_days = 3; # the max age in days to keep a logfile
           max_backups = 2; # the max number of rolled files to keep
-          compress = true; # determines if the rotated log files should be compressed using gzip. The default is false
+          compress = true; # determines if the rotated log files should be compressed using gzip. the default is false
         };
+
       };
     };
   };
-  scripts."show_logs".exec = "${lib.getExe pkgs.tailspin} -f .devenv/process-compose.log";
+  scripts."show_logs".exec = "tail -f .devenv/process-compose.log";
   # call devenv processes down after terminating the logs
   scripts."pup".exec = ''
     down_processes() {
